014fd4f03a1dc0efa1d9693851b2bdbbcc0b2273
gh0st <1264131+gh0st42@users.noreply.github.com>
2024-03-07 22:55:53 +0100

    fix: updated dtn7-showroom.sh to use prebuild container from dockerhub and support for podman

--- dtn7-showroom.sh
+++ dtn7-showroom.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 clear
+
+DOCKERCMD=docker
+# use podman if available
+if command -v podman &> /dev/null; then
+  DOCKERCMD=podman
+fi
+
+SHARED=shared
+# check is first parameter is not empty 
+if [ -n "$1" ]; then
+    SHARED=$1
+fi
+
+if [ ! -d "$SHARED" ]; then
+    mkdir -p $SHARED
+fi
+
 echo
 echo "'########::'########:'##::: ##:'########::'######::'##::::'##::'#######::'##:::::'##:'########:::'#######:::'#######::'##::::'##:"
 echo " ##.... ##:... ##..:: ###:: ##: ##..  ##:'##... ##: ##:::: ##:'##.... ##: ##:'##: ##: ##.... ##:'##.... ##:'##.... ##: ###::'###:"
@@ -13,7 +30,9 @@ echo
 echo "==> vnc://127.0.0.1:5901"
 echo "==> password: sneakers"
 echo 
-docker run --rm -it                                 \
+echo "==> Shared folder: $SHARED"
+echo
+$DOCKERCMD run --rm -it                             \
     --name showroom                                 \
     -p 5901:5901                                    \
     -p 50052:50051                                  \
@@ -21,5 +40,4 @@ docker run --rm -it                                 \
     -p 1190:1190                                    \
     -v $SHARED:/shared                              \
     --privileged                                    \
-    $INTERACTIVE                                    \
-    dtn7-showroom
+    gh0st42/dtn7-showroom
